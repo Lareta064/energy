af31733377bb289367766e7f059ff3f993bae84c
maryka <marykaweb@gmail.com>
2020-01-11 00:09:17 +0300

    aside mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 
+    let gumburger = document.querySelector('.fa-bars');
+    let aside = document.querySelector('.aside');
+
+    gumburger.addEventListener('click', function(){
+        aside.classList.toggle('show');
+    });
 
 })
