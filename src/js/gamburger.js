d4f73ae9c81a9ff9149b88a55ddcf3c592389b55
Lareta <lareta064@gmail.com>
2020-01-11 17:25:50 +0300

    mob-menu

---
+++ src/js/gamburger.js
@@ -0,0 +1,39 @@
+(function () {
+
+    "use strict";
+
+    var toggles = document.querySelectorAll(".cmn-toggle-switch");
+
+    for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+    };
+
+    function toggleHandler(toggle) {
+        toggle.addEventListener("click", function (e) {
+            e.preventDefault();
+            (this.classList.contains("active") === true) ? this.classList.remove("active"): this.classList.add("active");
+        });
+    }
+    //-показать мобильное меню
+    const bodyElement = document.querySelector('body')
+    const mobileMenuToggle = document.querySelector('#nav-menu-toggle')
+    const mobileMenu = document.querySelector('.aside')
+
+    mobileMenuToggle.addEventListener('click', function () {
+        if (this.classList.contains('active')) {
+            mobileMenu.classList.add('show')
+            bodyElement.classList.add('noscroll')
+
+        } else {
+            mobileMenu.classList.remove('show')
+            bodyElement.classList.remove('noscroll')
+        }
+    })
+
+    // mobileMenu.addEventListener('click', function () {
+    //     this.classList.remove('active')
+    //     mobileMenuToggle.classList.remove('active')
+    //     bodyElement.classList.remove('noscroll')
+    // })
+})();
