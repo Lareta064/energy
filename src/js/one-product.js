6e27086df77248721657189dd4723a849978cc0d
maryka <marykaweb@gmail.com>
2020-01-13 20:48:08 +0300

    one product page 2048

--- src/js/one-product.js
+++ src/js/one-product.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    $('.benefits-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
+        centerPadding: '60px',
+        autoplay: true,
+        prevArrow: '<span class="slick-arrow--custom-left"><i class="fas fa-angle-left"></span>',
+        nextArrow: '<span class="slick-arrow--custom-right" ><i class="fas fa-angle-right"></span>'
+
+    });
     // Страница one-product скрытие и пока второго блока
     let textall = document.getElementById('textall');
     let textroll = document.getElementById('textroll');
