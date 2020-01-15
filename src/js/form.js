7a017fd0fc8ed1700c518c7d980892bf6c5d94e2
Lareta <lareta064@gmail.com>
2020-01-15 16:22:39 +0300

    modal

--- src/js/form.js
+++ src/js/form.js
@@ -1,25 +1,50 @@
 $(document).ready(function () {
-    $('.benefits-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        variableWidth: true,
-        centerPadding: '60px',
-        autoplay: true,
-        prevArrow: '<span class="slick-arrow--custom-left"><i class="fas fa-angle-left"></span>',
-        nextArrow: '<span class="slick-arrow--custom-right" ><i class="fas fa-angle-right"></span>'
+    //-PLACEHOLDER
+    const formRows = document.querySelectorAll('.form-row')
+    const formRowsInputs = document.querySelectorAll('input')
 
-    });
+    for (let i = 0; i < formRows.length; i++) {
 
+        formRows[i].addEventListener('click', function () {
+            const placeholderElement = this.querySelector('.form-row__fake-placeholder')
+            placeholderElement.classList.add('active')
 
-    //читать далее блок Партнеры
-    const btnMoreInfo = document.querySelector('.more-info');
-    const hideInfo = document.querySelector('.section-paragraph--hide');
+        })
 
-    btnMoreInfo.addEventListener('click', function () {
-        hideInfo.style.display = 'block';
-        this.style.display = 'none'
+    }
+
+    for (let i = 0; i < formRowsInputs.length; i++) {
+        formRowsInputs[i].addEventListener('blur', function () {
+            const thisFakePlaceholder = this.nextElementSibling
+            if (this.value == '') {
+                thisFakePlaceholder.classList.remove('active')
+            }
+            if (this.value.length > 0) {
+                this.classList.add('valid')
+            }
+        })
+
+    }
+
+    document.querySelector('textarea').addEventListener('blur', function () {
+
+        if (this.value.length > 0) {
+            this.classList.add('valid')
+        }
     })
+    const callBtn = document.querySelector('.aside-footer-callback')
+    const priceBtn = document.querySelector('.aside-footer-price')
+    const callModal = document.querySelector('.modal-call')
+    const closeModal = document.querySelector('.close-modal')
+
+    callBtn.addEventListener('click', function () {
+        callModal.classList.add('show')
+    })
+
+    closeModal.addEventListener('click', function () {
+        callModal.classList.remove('show')
+    })
+
 
     //подкатегории в моб меню
     const mobCategory = document.querySelectorAll('.mobile-item-category');
@@ -49,24 +74,19 @@ $(document).ready(function () {
     let searchHeader = document.querySelector('.header-search');
     let searchInput = document.querySelector('.header-form-input');
     searchForm.addEventListener('click', function (e) {
-        e.preventDefault()
         $target = $(e.target);
         if (searchInput.classList.contains('search-open') && searchInput.value == '' && !$target.hasClass('header-form-input')) {
             searchInput.classList.remove('search-open');
             searchForm.classList.remove('search-open');
             searchHeader.classList.remove('header-search-open');
-
         } else if (searchInput.classList.contains('search-open') && searchInput.value != '') {
             searchForm.submit();
-
-
         } else {
             searchInput.classList.add('search-open');
             searchForm.classList.add('search-open');
             searchHeader.classList.add('header-search-open');
 
-
-
         }
     });
+
 })
